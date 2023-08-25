208f9907461e24fbf4b93ac03e5ea38fde6b8cbd
Pedro Afonso Dornelas Marcato <pamarcato@stefanini.com>
2023-08-25 17:23:47 -0300

    Criação da hospitalController

---
+++ VITAE/src/main/java/org/example/Controller/HospitalController.java
@@ -0,0 +1,36 @@
+package org.example.Controller;
+
+import org.example.Domain.Hospital;
+import org.example.Records.Hospital.AtualizarHospital;
+import org.example.Records.Hospital.RecordHospital;
+import org.example.interfaces.HospitalRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hospital")
+public class HospitalController {
+
+    private HospitalRepository repository;
+    @GetMapping
+    public ResponseEntity<List<Hospital>> listar(){
+        return ResponseEntity.status(200).body(repository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity cadastrar(@RequestBody RecordHospital dados){
+        return ResponseEntity.status(201).body(repository.save(new Hospital(dados)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity atualizar(@PathVariable Long id , @RequestBody AtualizarHospital dados){
+        if (repository.existsById(id)){
+            var selectHospital = repository.getReferenceById(id);
+            selectHospital.AtualizaHospital(dados);
+            return ResponseEntity.status(200).body(new AtualizarHospital(selectHospital));
+        }
+        return ResponseEntity.status(404).build();
+    }
+}
--- VITAE/src/main/java/org/example/Domain/Hospital.java
+++ VITAE/src/main/java/org/example/Domain/Hospital.java
@@ -2,6 +2,9 @@ package org.example.Domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.example.Records.Hospital.AtualizarHospital;
+import org.example.Records.Hospital.RecordHospital;
+
 @Table(name = "Hospital")
 @Entity(name = "Hospital")
 @Getter
@@ -14,7 +17,21 @@ public class Hospital {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    private String Email;
+    private String email;
     private String senha;
+    private String cnpj;
 
+    public Hospital(RecordHospital cadastroHospital) {
+        this.nome = cadastroHospital.nome();
+        this.email = cadastroHospital.email();
+        this.senha = cadastroHospital.senha();
+        this.cnpj = cadastroHospital.cnpj();
+    }
+
+    public void AtualizaHospital(AtualizarHospital atualizarDados) {
+        this.nome = atualizarDados.nome();
+        this.email = atualizarDados.email();
+        this.senha = atualizarDados.senha();
+        this.cnpj = atualizarDados.cnpj();
+    }
 }
