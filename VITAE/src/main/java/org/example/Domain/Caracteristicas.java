10cff09b9f918ca59ff045c36fd6b7a73b3edb26
theviniciosgarcia <vinicios.fagundes@sptech.school>
2023-08-28 11:37:59 -0300

    incrmentando a versão com a funcionalidade de caracteristicas

--- VITAE/src/main/java/org/example/Domain/Caracteristicas.java
+++ VITAE/src/main/java/org/example/Domain/Caracteristicas.java
@@ -2,6 +2,8 @@ package org.example.Domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.example.Records.Caracteristicas.AtualizaCaracteristicas;
+import org.example.Records.Caracteristicas.RecordCaracteristicas;
 
 import java.util.Date;
 
@@ -16,12 +18,28 @@ public class Caracteristicas {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Double peso;
-    private Double altura;
+    private String peso;
+    private String altura;
     private boolean tatto;
     private String sexo;
     private Date dtNascimento;
     private Long fkUsuario;
 
-   
+    public Caracteristicas(RecordCaracteristicas dados) {
+        this.peso = dados.peso();
+        this.altura = dados.altura();
+        this.tatto = dados.tatto();
+        this.sexo = dados.sexo();
+        this.dtNascimento = dados.dtNascimento();
+        this.fkUsuario = dados.fkUsuario();
+    }
+
+    public void AtualizaCaracteristicas(AtualizaCaracteristicas dados) {
+        this.peso = dados.peso();
+        this.altura = dados.altura();
+        this.tatto = dados.tatto();
+        this.sexo = dados.sexo();
+        this.dtNascimento = dados.dtNascimento();
+        this.fkUsuario = dados.fkUsuario();
+    }
 }
