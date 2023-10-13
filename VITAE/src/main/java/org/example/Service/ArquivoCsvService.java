e23cae21e20db5e0be3f212442fc0a61a3daab01
pedroafonso0728 <pedro.marcato@sptech.school>
2023-10-13 13:42:05 -0300

    Arquivo txt e csv com data BR + Ordenação

--- VITAE/src/main/java/org/example/Service/ArquivoCsvService.java
+++ VITAE/src/main/java/org/example/Service/ArquivoCsvService.java
@@ -10,6 +10,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Formatter;
 import java.util.FormatterClosedException;
 import java.util.List;
@@ -22,12 +23,28 @@ public class ArquivoCsvService {
     @Autowired
     private AgendaRepository serviceRepository;
 
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+
     public void recupararValores() {
 
         List<RecuperarValoresAgendamento> resultados = serviceRepository.buscaDoadorAgendado();
         List<AgendamentoDTO> testes = resultados.stream()
                 .map(result -> new AgendamentoDTO(result.getNome(), result.getHorario()))
                 .collect(Collectors.toList());
+
+        for (int i = 0; i < testes.size() - 1; i++) {
+            for (int j = i + 1; j < testes.size(); j++) {
+                int hora1 = testes.get(i).getHorario().getHour();
+                int hora2 = testes.get(j).getHorario().getHour();
+                if (hora1 > hora2) {
+                    AgendamentoDTO aux = testes.get(i);
+                    testes.set(i, testes.get(j));
+                    testes.set(j, aux);
+                }
+            }
+        }
+
         gravaArquivoCsv(testes,"Agendamentos csv Do Dia");
         gravaArquivoTxt(testes,"Agendamentos txt Do Dia");
     }
@@ -37,13 +54,13 @@ public class ArquivoCsvService {
         Formatter saida = null;
         Boolean deuRuim = false;
 
-        nomeArq += LocalDate.now().getDayOfMonth()  + ".csv";
+        nomeArq += LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + ".csv";
 
         try {
             arq = new FileWriter(nomeArq);
             saida = new Formatter(arq);
-        }
-        catch (IOException erro) {
+            saida.format("%s,%s\n", "NOME:", "HORÁRIO:");
+        } catch (IOException erro) {
             System.out.println("Erro ao abrir o arquivo");
             System.exit(1);
         }
@@ -51,20 +68,18 @@ public class ArquivoCsvService {
         try {
             for (int i = 0; i < lista.size(); i++) {
                 AgendamentoDTO agendamento = lista.get(i);
-                saida.format("%s;%s\n",agendamento.getNome(), agendamento.getHorario());
+                String horarioFormatado = agendamento.getHorario().format(formatter); // Formatar a data e hora
+                saida.format("%s;%s\n", agendamento.getNome(), horarioFormatado);
             }
-        }
-        catch (FormatterClosedException erro) {
+        } catch (FormatterClosedException erro) {
             System.out.println("Erro ao gravar o arquivo");
             erro.printStackTrace();
             deuRuim = true;
-        }
-        finally {
+        } finally {
             saida.close();
             try {
                 arq.close();
-            }
-            catch (IOException erro) {
+            } catch (IOException erro) {
                 System.out.println("Erro ao fechar o arquivo");
                 deuRuim = true;
             }
@@ -79,11 +94,12 @@ public class ArquivoCsvService {
         Formatter saida = null;
         Boolean deuRuim = false;
 
-        nomeArq += LocalDate.now().getDayOfMonth() + ".txt";
+        nomeArq += LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + ".txt";
 
         try {
             arq = new FileWriter(nomeArq);
             saida = new Formatter(arq);
+            saida.format("%s,%s\n", "NOME", "HORÁRIO:");
         } catch (IOException erro) {
             System.out.println("Erro ao abrir o arquivo");
             System.exit(1);
@@ -92,7 +108,8 @@ public class ArquivoCsvService {
         try {
             for (int i = 0; i < lista.size(); i++) {
                 AgendamentoDTO agendamento = lista.get(i);
-                saida.format("%s;%s\n", agendamento.getNome(), agendamento.getHorario());
+                String horarioFormatado = agendamento.getHorario().format(formatter); // Formatar a data e hora
+                saida.format("%s;%s\n", agendamento.getNome(), horarioFormatado);
             }
         } catch (FormatterClosedException erro) {
             System.out.println("Erro ao gravar o arquivo");
@@ -112,3 +129,4 @@ public class ArquivoCsvService {
         }
     }
 }
+
