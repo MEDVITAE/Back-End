eadb003841f77b9bf5064768cae76a6971e91e73
MaessioSptech <maessio.sousa@sptech.school>
2024-03-21 17:27:20 -0300

    ajustante porta

--- VITAE/src/main/java/org/example/Configuration/Cors.java
+++ VITAE/src/main/java/org/example/Configuration/Cors.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Cors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods(HttpMethod.GET.name(),
+        registry.addMapping("/http://18.205.233.220:3000/login").allowedMethods(HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
                 HttpMethod.PUT.name(),
                 HttpMethod.PATCH.name(),
